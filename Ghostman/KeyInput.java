584be36190f567377af45de52065561a7d2a1716
Fernando Samano <SamanoF21@gmail.com>
2019-11-06 12:48:13 -0800

    Optimized

--- Ghostman/KeyInput.java
+++ Ghostman/KeyInput.java
@@ -11,66 +11,38 @@ public class KeyInput extends KeyAdapter {
 	public KeyInput(Handler handler) {
 		this.handler = handler;
 	}
+
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		//Ghost Keyinput was too fast and had to be slow down
+		// Ghost Keyinput was too fast and had to be slow down
 		if ((System.currentTimeMillis() - lastPressProcessed) > 700) {
 			lastPressProcessed = System.currentTimeMillis();
-			for (GameObject obj:handler.object) {
+			for (GameObject obj : handler.getGameObjects()) {
 				if (obj instanceof Ghost) {
 					Ghost g = (Ghost) obj;
-					if (Game.state==Game.GameState.PLAYING) {
+					if (Game.state == Game.GameState.PLAYING) {
 						if (key == KeyEvent.VK_RIGHT) {
 
-							if (!handler.hasObject(g.getListX() + 1, g.getListY(),GameObject.ObjectId.Block)) {
+							if (!handler.hasObject(g.getListX() + 1, g.getListY(), GameObject.ObjectId.Block)) {
 								handler.moveObject(Pac.Direction.Right, obj);
-								if(handler.object.removeIf(o -> o.getId() == GameObject.ObjectId.Pac
-										&& o.getListX() == g.getListX() && o.getListY() == g.getListY()))
-								{
-									Pac.pacCount--;
-									Pac.enemyCount--;
-								}
 							}
-
 						} else if (key == KeyEvent.VK_LEFT) {
-							if (!handler.hasObject(g.getListX() - 1, g.getListY(),GameObject.ObjectId.Block)) {
+							if (!handler.hasObject(g.getListX() - 1, g.getListY(), GameObject.ObjectId.Block)) {
 								handler.moveObject(Pac.Direction.Left, obj);
-								if(handler.object.removeIf(o -> o.getId() == GameObject.ObjectId.Pac
-										&& o.getListX() == g.getListX() && o.getListY() == g.getListY()))
-								{
-									Pac.pacCount--;
-									Pac.enemyCount--;
-								}
 							}
 						} else if (key == KeyEvent.VK_UP) {
-							if (!handler.hasObject(g.getListX(), g.getListY() - 1,GameObject.ObjectId.Block)) {
+							if (!handler.hasObject(g.getListX(), g.getListY() - 1, GameObject.ObjectId.Block)) {
 								handler.moveObject(Pac.Direction.Up, obj);
-								if(handler.object.removeIf(o -> o.getId() == GameObject.ObjectId.Pac
-										&& o.getListX() == g.getListX() && o.getListY() == g.getListY()))
-								{
-									Pac.pacCount--;
-									Pac.enemyCount--;
-								}
 							}
 						} else if (key == KeyEvent.VK_DOWN) {
-							if (!handler.hasObject(g.getListX(), g.getListY() + 1,GameObject.ObjectId.Block)) {
+							if (!handler.hasObject(g.getListX(), g.getListY() + 1, GameObject.ObjectId.Block)) {
 								handler.moveObject(Pac.Direction.Down, obj);
-								
-								if(handler.object.removeIf(o -> o.getId() == GameObject.ObjectId.Pac
-										&& o.getListX() == g.getListX() && o.getListY() == g.getListY()))
-								{
-									Pac.pacCount--;
-									Pac.enemyCount--;
-								}
-								
 							}
-						}
-						else if(key=='p'||key=='P')
-						{
-							Game.state=Game.GameState.PAUSED;
+						} else if (key == 'p' || key == 'P') {
+							Game.state = Game.GameState.PAUSED;
 						}
 					} else if (key == 'p' || key == 'P') {
-						Game.state=Game.GameState.PLAYING;
+						Game.state = Game.GameState.PLAYING;
 					}
 
 				}
