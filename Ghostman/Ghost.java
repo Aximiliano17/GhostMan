bb122e09176f57a21049b49ed5754564349e746d
Fernando Samano <SamanoF21@gmail.com>
2020-05-04 23:02:41 -0700

    Modified Ghost input.
    Before you had to constantly press the key in the direction you wanted the Ghost to move, now it moves to one direction, unless the direction is changed by the input.

--- Ghostman/Ghost.java
+++ Ghostman/Ghost.java
@@ -9,6 +9,7 @@ import javax.swing.ImageIcon;
 public class Ghost extends GameObject {
 
 	private String ghost = "/images/Ghost1.gif";
+	private Pac.Direction dir=null;
 	
 	public Ghost(ObjectId id,int mapX,int mapY) {
 		super(id,mapX,mapY);
@@ -17,10 +18,20 @@ public class Ghost extends GameObject {
 	public void render(Graphics g) {
 		g.drawImage(getImages(ghost), (int) x, (int) y, null);
 	}
-
 	public Image getImages(String s) {
 		ImageIcon i = new ImageIcon(getClass().getResource(s));
 		return i.getImage();
+	}
+	public void moveGhost(Handler handler)
+	{
+		if(this.dir==null) return;
+		
+		moveObject(dir,this,handler);
+		
+	}
+	public void setDir(Pac.Direction dir)
+	{
+		this.dir=dir;
 	}
 
 }
